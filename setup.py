4bb50fbb0c7a46abc6fc32adf4460c95082b7481
aboutmydreams <aboutmydreams@163.com>
2024-09-21 08:24:19 +0800

    fix: get_modes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycapt",
-    version="1.0.8",
+    version="1.0.10",
     author="aboutmydreams",
     author_email="aboutmydreams@163.com",
     description="a library that processes verification codes",
